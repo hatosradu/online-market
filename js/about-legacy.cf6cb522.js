cc487353c3f8fdaa517423d7e6abb1c739031760
Radu Hatos <hatosradu@gmail.com>
2022-05-21 10:11:10 +0300

    products-view

--- js/about-legacy.cf6cb522.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunke_market"]=self["webpackChunke_market"]||[]).push([[443],{9030:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var u=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],r=n(1001),s={},l=(0,r.Z)(s,u,a,!1,null,null,null),c=l.exports}}]);
-//# sourceMappingURL=about-legacy.cf6cb522.js.map
